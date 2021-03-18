0d36408b19d341e0bbecb8bbf5e6fa14b14a340d
Louis <l.g.coreas@gmail.com>
2021-03-18 13:46:39 -0400

    project set up

---
+++ server/database.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE PERNJWT;
+
+CREATE TABLE users(
+  user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  user_name VARCHAR(255) NOT NULL,
+  user_email VARCHAR(255) NOT NULL UNIQUE,
+  user_password VARCHAR(255) NOT NULL,
+  PRIMARY KEY(user_id)
+  );
+CREATE TABLE todo(
+  todo_id SERIAL,
+  user_id UUID ,
+  description VARCHAR(255),
+  PRIMARY KEY (todo_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+  INSERT INTO users(user_name, user_email, user_password) 
+  VALUES ('LOUIS COREAS', 'fake@fake.com', 'newpass123');
---
+++ server/db.js
@@ -0,0 +1,12 @@
+const Pool = require("pg").Pool
+
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
+  host: "localhost",
+  port: 5432,
+  database: "PERNJWT"
+
+
+});
+module.exports = pool;
